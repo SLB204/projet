7b88a6f528dba4fbdf80e26286c3a3622e9364d5
SLB204 <81047373+SLB204@users.noreply.github.com>
2021-03-20 22:06:35 +0100

    Add files via upload

---
+++ projet 2.py
@@ -0,0 +1,19 @@
+class BlockChain(object):
+
+   def _init_(self):
+       self.chain = []
+       self.current_transaction = []
+
+   def new_block(self):
+       pass
+
+   def new_transaction(self):
+       pass
+
+
+   def hash(block):
+       pass
+
+
+   def last_block(self):
+       pass
